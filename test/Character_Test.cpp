ff3305ea2760427ab1e8a35488775ebaf3978ea2
Shaun O'Grady <shaun.m.ogrady@gmail.com>
2021-12-02 20:56:36 -0600

    googletest working

--- test/Character_Test.cpp
+++ test/Character_Test.cpp
@@ -1,15 +1,21 @@
 #include "gtest/gtest.h"
-//#include "../CPP_Quest/QuestCharacter.h"
-//#include "../CPP_Quest/QuestCharacter.cpp"
-//#include <tuple>
+#include "../CPP_Quest/QuestCharacter.h"
+#include <tuple>
 
 TEST(CharacterTest, test1) {
-    ////arrange
-    //std::tuple <std::string, std::string> lessonInfo;
-    //lessonInfo = std::make_tuple("Lesson1", "TheBasics");
-    ////act
-    //QuestCharacter testCharacter = QuestCharacter("test", "Application Developer", "Archer", 4, lessonInfo);
-    ////assert
-    //EXPECT_EQ(testCharacter.getExp(), 4);
+    //arrange
+    std::tuple <std::string, std::string> lessonInfo;
+    lessonInfo = std::make_tuple("Lesson1", "TheBasics");
+    //act
+    QuestCharacter testCharacter = QuestCharacter("test", "Application Developer", "Archer", 4, lessonInfo);
+    //assert
+    EXPECT_EQ(testCharacter.getExp(), 4);
     EXPECT_EQ(true, true);
 }
+
+//#include <gtest/gtest.h>
+//
+//TEST(HelloTest, BasicAssertions) {
+//    EXPECT_STRNE("hello", "world");
+//    EXPECT_EQ(7 * 6, 42);
+//}
